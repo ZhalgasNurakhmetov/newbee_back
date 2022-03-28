b2871527b0a997402621087804e1df61e2382fd7
Zhalgas Nurakhmetov <z.nurakhmetov@kazdream.kz>
2022-03-28 14:15:11 +0600

    refactor config

--- config/config.py
+++ config/config.py
@@ -6,7 +6,9 @@ class DevelopmentConfig(BaseSettings):
     DEBUG = True
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY: str = os.getenv('START_UP_KEY')
+    ALGORITHM: str = "HS256"
     SQLALCHEMY_DATABASE_URI: str = os.getenv('DATABASE_URI_COM')
+    ACCESS_TOKEN_EXPIRE_WEEKS: int = 1
 
 
 config = DevelopmentConfig()
