a29e1148f8573c54c50c090bac9053e088ea5511
Zhalgas Nurakhmetov <z.nurakhmetov@kazdream.kz>
2022-03-28 14:15:54 +0600

    add categories to database

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
 from fastapi import FastAPI
 from starlette.middleware.cors import CORSMiddleware
 
-from database.database import Base, engine
+from database.database import Base, engine, categories
 from routes.routes import initialize_routes
 
 Base.metadata.create_all(engine, checkfirst=True)
+
+# TODO uncomment before production
+
+# with engine.connect() as con:
+#     statement = text("""INSERT INTO categories (groupc, title) VALUES (:groupc, :title)""")
+#     for category in categories:
+#         con.execute(statement, **category)
 
 app = FastAPI(title="Newbee server", version="0.1.0")
 
